ff25a0a74dd594f909c06889cdae26e9e90de3fd
Mahmudujjaher Tonmoy <troxin.team@gmail.com>
2025-05-21 17:48:48 +0600

    Update help.js

--- modules/commands/help.js
+++ modules/commands/help.js
@@ -175,7 +175,6 @@ module.exports.run = async function ({ api, event, args, getText }) {
     const config = require("./../../config.json")
     const msgg = {
   body: `╭──────────────╮\n│ 𝖢𝗈𝗆𝗆𝖺𝗇𝖽/𝖢𝖺𝗍𝖾𝗀𝗈𝗋𝗒 │\n╰──────────────╯\n‣ Bot Owner: ${config.DESIGN.Admin}\n\n` + msg + `\n◖Total pages available: ${totalPages}.\n` + `\n╭ ────── ╮\n│    GUIDE    │\n╰ ────── ╯\n` + getText("guideList", config.PREFIX),
-  attachment: imgP,
 };
 
     const sentMessage = await api.sendMessage(msgg, threadID, messageID);
